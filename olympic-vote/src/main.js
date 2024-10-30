9b3b13539b9aa34dde53b552af3a1a61c8b1be0f
forExpecto <1838844858@qq.com>
2024-10-30 14:42:54 +0800

    更新路由与ELement引入

--- olympic-vote/src/main.js
+++ olympic-vote/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import router from '@/router/index.js'
+import 'element-plus/theme-chalk/index.css'
+import './assets/css/index.css'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(router)
+    .use(ElementPlus)
+    .mount('#app')
